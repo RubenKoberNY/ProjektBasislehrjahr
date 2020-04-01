e9c59537564c4d867fc06f2b4d6b802ff7cad393
JanBloch <jan@bhome.ch>
2020-04-01 15:02:57 +0200

    Added function getAllQuestions() in risiko/script.js Added function sendQuestions() in risiko/script.js

--- PHP/templates/risiko/script.js
+++ PHP/templates/risiko/script.js
@@ -1,36 +1,45 @@
+var questions;
+var result;
+
 //region Prototypes
 HTMLElement.prototype.replaceHTML = function (search, replacement) {
     this.innerHTML = this.innerHTML.replace(search, replacement);
 }
 //endregion
 
-//region Questions
-function getAllQuestions(){
-    //ToDo: get all questions
-}
-//endregion
+questions = getAllQuestions();
+questions = [{
+    question: "Angenommen, Sie können 20000 € anlegen: Welche Stragetie wählen Sie?",
+    answers: [{
+        "id": "ans1",
+        "value": "Zu je 50% Wahrscheinlichkeit haben Sie nach 5 Jahren 20000 oder 22000"
+    }, {"id": "ans2", "value": "Zu je 50% Wahrscheinlichkeit haben Sie nach 5 Jahren 19000 oder 23000"}, {
+        "id": "ans3",
+        "value": "Zu je 50% Wahrscheinlichkeit haben Sie nach 5 Jahren 18000 oder 24000"
+    }, {"id": "ans4", "value": "Zu je 50% Wahrscheinlichkeit haben Sie nach 5 Jahren 12000 oder 30000"}]
+}];
+renderCurrentQuestion();
 
-//region Questions Test Object
-var questions = [
-    {
-        question:"Angenommen, Sie können 20000 € anlegen: Welche Stragetie wählen Sie?",
-        answers: [
-            {
-                "id": "ans1",
-                "value": "Zu je 50% Wahrscheinlichkeit haben Sie nach 5 Jahren 20000 oder 22000"
-            }, {
-                "id": "ans2",
-                "value": "Zu je 50% Wahrscheinlichkeit haben Sie nach 5 Jahren 19000 oder 23000"
-            }, {
-                "id": "ans3",
-                "value": "Zu je 50% Wahrscheinlichkeit haben Sie nach 5 Jahren 18000 oder 24000"
-            }, {
-                "id": "ans4",
-                "value": "Zu je 50% Wahrscheinlichkeit haben Sie nach 5 Jahren 12000 oder 30000"
-            }
-        ]
-    }
-];
+//region Questions
+function getAllQuestions() {
+    $.ajax({
+        url: "/api/quiz/risiko/get",
+        method: "GET"
+    }).done(function (result) {
+        return JSON.parse(result);
+    });
+}
+
+function sendQuestions() {
+    $.ajax({
+        url: "/api/quiz/risiko/post",
+        method: "POST",
+        data: result
+    }).done(function (result) {
+        console.log(result);
+    });
+}
+
 //endregion
 
 
@@ -45,6 +54,4 @@ function renderCurrentQuestion() {
         document.getElementById(answers[i].id).replaceHTML("%%" + answers[i].id + "%%", answers[i].value);
     }
 }
-
-renderCurrentQuestion();
 //endregion
