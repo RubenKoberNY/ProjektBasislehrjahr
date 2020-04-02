d8bdd7528c328137ee2786a2e8bae212abbae99a
JanBloch <jan@bhome.ch>
2020-04-02 09:51:08 +0200

    Changed SQL statements in RisikoRepository.php

--- PHP/repositories/RisikoRepository.php
+++ PHP/repositories/RisikoRepository.php
@@ -1,31 +1,28 @@
 <?php
 
+if (!defined("MYSQLI_ASSOC"))
+    define("MYSQLI_ASSOC", 1);
 
 class RisikoRepository
 {
-
-    const ID_QUIZ = 4;
-
     public function __construct()
     {
     }
 
     public function getAllQuestions()
     {
-        $sql = "CALL getFrageFromQuizId(?)";
+        $sql = "CALL risiko_getAllFrage()";
         $stmt = DB::getInstance()->prepare($sql);
-        $id = self::ID_QUIZ;
-        $stmt->bind_param("i", $id);
         $stmt->execute();
         return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
     }
 
     public function getAnswersFromQuestionId($id)
     {
-        $sql = "CALL getAntwortFromFrageId(?)";
+        $sql = "CALL risiko_getAntwortFromFrageId(?)";
         $stmt = DB::getInstance()->prepare($sql);
         $stmt->bind_param("i", $id);
         $stmt->execute();
-        return $stmt->get_result()->fetch_assoc();
+        return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
     }
 }
