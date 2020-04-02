d43e5ea3a74be5c33e5ee09ed5100859736c9c21
JanBloch <jan@bhome.ch>
2020-04-02 08:25:52 +0200

    Added /api/risiko/get endpoint Added method getAllQuestions in RisikoRepository.php Added method getAnswersFromQuestionId in RisikoRepository.php Added router array in IdController.php

--- PHP/repositories/RisikoRepository.php
+++ PHP/repositories/RisikoRepository.php
@@ -3,7 +3,29 @@
 
 class RisikoRepository
 {
+
+    const ID_QUIZ = 4;
+
     public function __construct()
     {
+    }
+
+    public function getAllQuestions()
+    {
+        $sql = "CALL getFrageFromQuizId(?)";
+        $stmt = DB::getInstance()->prepare($sql);
+        $id = self::ID_QUIZ;
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getAnswersFromQuestionId($id)
+    {
+        $sql = "CALL getAntwortFromFrageId(?)";
+        $stmt = DB::getInstance()->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        return $stmt->get_result()->fetch_assoc();
     }
 }
