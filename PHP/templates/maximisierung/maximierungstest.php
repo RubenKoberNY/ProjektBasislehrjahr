80be60b40e68b184f4fc1c312b115594e26765be
Nicolas <beroudnicolas10@gmail.com>
2020-04-02 10:44:30 +0200

    Erste Seite des Maximierungstest + der Maximierungstest + ein bisschen CSS

--- PHP/templates/maximisierung/maximierungstest.php
+++ PHP/templates/maximisierung/maximierungstest.php
@@ -1,9 +1,5 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
     <head>
         <meta charset="UTF-8">
         <title>Maximierungstest</title>
@@ -28,13 +24,13 @@
             <label>6</label>
             <input class="radio" type="radio" name="Zahl" value="7">
             <label>7</label>
+            </div>
         </form>
-    </div>
     <br>
 
         <p class="frage-white-text">2. Wenn ich im Auto Radio höre, wechsle ich oft den Sender.<br> Ich will wissen, ob irgendwo etwas Besseres läuft - auch wenn ich mit dem, was ich gerade höre, zufrieden bin.</p>
-        <form>
-            <div class="div_radio">
+    <form>
+        <div class="div_radio">
             <input class="radio" type="radio" name="Zahl" value="1">
             <label>1</label>
             <input class="radio" type="radio" name="Zahl" value="2">
@@ -49,13 +45,13 @@
             <label>6</label>
             <input class="radio" type="radio" name="Zahl" value="7">
             <label>7</label>
-        </form>
         </div>
+    </form>
     <br>
 
         <p class="frage-white-text">3. Es fällt mir schwer, Geschenke für Freunde zu finden.</p>
-        <form>
-            <div class="div_radio">
+    <form>
+        <div class="div_radio">
             <input class="radio" type="radio" name="Zahl" value="1">
             <label>1</label>
             <input class="radio" type="radio" name="Zahl" value="2">
@@ -70,13 +66,13 @@
             <label>6</label>
             <input class="radio" type="radio" name="Zahl" value="7">
             <label>7</label>
-        </form>
-    </div>
+        </div>
+    </form>
     <br>
 
         <p class="frage-white-text">4. In der Videothek (oder bei Netflix etc.) habe ich Schwierigkeiten, mich für den richtigen Film zu entscheiden.</p>
-        <form>
-            <div class="div_radio">
+    <form>
+        <div class="div_radio">
             <input class="radio" type="radio" name="Zahl" value="1">
             <label>1</label>
             <input class="radio" type="radio" name="Zahl" value="2">
@@ -91,13 +87,13 @@
             <label>6</label>
             <input class="radio" type="radio" name="Zahl" value="7">
             <label>7</label>
-        </form>
         </div>
+    </form>
     <br>
 
         <p class="frage-white-text">5. Ich begnüge mich nie mit dem Zweitbesten.</p>
-        <form>
-            <div class="div_radio">
+    <form>
+        <div class="div_radio">
             <input class="radio" type="radio" name="Zahl" value="1">
             <label>1</label>
             <input class="radio" type="radio" name="Zahl" value="2">
@@ -112,13 +108,13 @@
             <label>6</label>
             <input class="radio" type="radio" name="Zahl" value="7">
             <label>7</label>
-        </form>
-    </div>
+        </div>
+    </form>
     <br>
 
         <p class="frage-white-text"> 6. Bei allem, was ich mache, habe ich die höchsten Ansprüche</p>
-        <form>
-            <div class="div_radio">
+    <form>
+        <div class="div_radio">
             <input class="radio" type="radio" name="Zahl" value="1">
             <label>1</label>
             <input class="radio" type="radio" name="Zahl" value="2">
@@ -133,8 +129,8 @@
             <label>6</label>
             <input class="radio" type="radio" name="Zahl" value="7">
             <label>7</label>
-        </form>
         </div>
+    </form>
     <br><br>
 
 
