3dbdd54d45419d74f5d397ff1cb138129c696ef5
JanBloch <jan@bhome.ch>
2020-04-02 16:39:53 +0200

    Added method insertUseranswer in RisikoRepository.php

--- PHP/repositories/RisikoRepository.php
+++ PHP/repositories/RisikoRepository.php
@@ -26,11 +26,21 @@ class RisikoRepository
         return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function insertResult($uid, $user_answer_id, $quiz_id, $gamecode_id, $score_answer_id)
+    public function insertResult($uid, $quiz_id, $gamecode_id)
     {
-        $sql = "INSERT INTO resultat(benutzer_id, benutzerantwort_id, quiz_id, gameid_id, punktzahlantwort_id) VALUES(?, ?, ?, ?, ?);";
+        $sql = "INSERT INTO resultat(benutzer_id, quiz_id, gameid_id) VALUES(?, ?, ?);";
         $stmt = DB::getInstance()->prepare($sql);
-        $stmt->bindParam("iiiii", $uid, $user_answer_id, $quiz_id, $gamecode_id, $score_answer_id);
-        return $stmt->execute();
+        $stmt->bind_param("iii", $uid, $quiz_id, $gamecode_id);
+        $stmt->execute();
+        return $stmt->insert_id;
+    }
+
+    public function insertUserAnswer($answer_id, $user_id, $result_id)
+    {
+        $sql = "INSERT INTO benutzerantwort(antwort_id, benutzer_id, resultat_id) VALUES(?, ?, ?)";
+        $stmt = DB::getInstance()->prepare($sql);
+        $stmt->bind_param("iii", $answer_id, $user_id, $result_id);
+        $stmt->execute();
+        return $stmt->insert_id;
     }
 }
