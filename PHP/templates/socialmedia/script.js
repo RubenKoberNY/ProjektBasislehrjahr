3f9e5d25c1dd620a113c3dd423b4badb962ff810
McSlimy <romanetter16@gmail.com>
2020-04-06 16:14:58 +0200

    Socialmedia update

--- PHP/templates/socialmedia/script.js
+++ PHP/templates/socialmedia/script.js
@@ -4,25 +4,29 @@ function createQuestions(){
     for (let i = 1; i <= questions; i++){
 
         let fragedivid = "frage" + i;
+        let fragetext = "frage" + i + "frage";
         let radio1id = i + "radio1";
         let radio2id = i + "radio2";
-        let label1id = "lbl" + i + "radio1";
-        let label2id = "lbl" + i + "radio2";
+        let span1id = "spn" + i + "radio1";
+        let span2id = "spn" + i + "radio2";
+
+        let frage1 = document.getElementById(fragetext);
+        frage1.innerHTML = "[PLACEHOLDER FÜR FRAGE " + i + "]"
 
         let rdo1 = document.getElementById(radio1id);
         rdo1.setAttribute("name", fragedivid);
         rdo1.setAttribute("value", "Ja");
 
-        let lbl1 = document.getElementById(label1id);
-        lbl1.innerHTML = "Ja";
+        let spn1 = document.getElementById(span1id);
+        spn1.innerHTML = "Ja";
 
         let rdo2 = document.getElementById(radio2id);
         rdo2.setAttribute("name", fragedivid);
         rdo2.setAttribute("value", "Nein");
 
 
-        let lbl2 = document.getElementById(label2id);
-        lbl2.innerHTML = "Nein";
+        let spn2 = document.getElementById(span2id);
+        spn2.innerHTML = "Nein";
     }
 }
 createQuestions();
