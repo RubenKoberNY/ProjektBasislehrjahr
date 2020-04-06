ac6ad7b3cc9fed40064b70e4f1ce633e8d8e7fae
Nicolas <beroudnicolas10@gmail.com>
2020-04-06 11:23:22 +0200

    Maxierungsrepository erstellen

--- PHP/repositories/MaximisierungRepository.php
+++ PHP/repositories/MaximisierungRepository.php
@@ -1,9 +1,50 @@
 <?php
 
+if (!defined( "MYSQLI_ASSOC"))
+    define("MYSQLI_ASSOC", 1);
 
 class MaximisierungRepository
 {
     public function __construct()
     {
     }
+
+    public function getQuestions()
+    {
+        $sql = "CALL maximierung_getQuestions()";
+        $statement = DB::getInstance()->prepare($sql);
+        $statement->execute();
+        return $statement->get_result()->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getAnswersFromQuestionId($id)
+    {
+        $sql = "CALL maximierung_getAnswersFromQuestionId(?)";
+        $statement = DB::getInstance()->prepare($sql);
+        $statement->bind_param("i", $id);
+        $statement->execute();
+        return $statement->get_result()->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function insertResult($uid, $quiz_id, $gamecode_id)
+    {
+        $sql = "INSERT INTO resultat(benutzer_id, quiz_id, game_id) VALUES (?,?,?);";
+        $statement = DB::getInstance()->prepare($sql);
+        $statement->bind_param("iii", $uid,  $quiz_id, $gamecode_id);
+        $statement->execute();
+        return $statement->insert_id;
+    }
+
+    public function insertUserAnswer($answer_id, $user_id, $result_id)
+    {
+        $sql = "INSERT INTO benutzerantwort(antwort_id, benutzer_id, resultat_id) VALUES (?,?,?);";
+        $statement = DB::getInstance()->prepare($sql);
+        $statement->bind_param("iii", $answer_id,$user_id, $result_id);
+        $statement->execute();
+        return $statement->insert_id;
+    }
+
+
+
+
 }
