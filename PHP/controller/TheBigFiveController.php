ce4b467d2f645a36347f63854d4ed4aea31e5b08
hoi66_ <armin5s@icloud.com>
2020-04-07 08:54:39 +0200

    implemented dynamic questions, extended documentation

--- PHP/controller/TheBigFiveController.php
+++ PHP/controller/TheBigFiveController.php
@@ -5,5 +5,10 @@ class TheBigFiveController
 {
     public function __construct()
     {
+        $this->bigFiveRepository = new TheBigFiveRepository();
+    }
+    public function getAllQuestions() {
+        $questionArray = $this->bigFiveRepository->getAllQuestionsFromBigFive();
+        return json_encode($questionArray);
     }
 }
--- PHP/templates/thebigfive/script.js
+++ PHP/templates/thebigfive/script.js
@@ -1,25 +1,35 @@
-var array_of_dom_elements = document.querySelectorAll("input[type=range]");
-M.Range.init(array_of_dom_elements);
-let types = [
-    "extravertiert, offen",
-    "optimistisch"
-];
-let amountOfQuestions = types.length;
+var questions;
+let amountOfQuestions;
 var currentQuestion = 0;
+var givenAnswers = new Object();
 
 $( document ).ready(function() {
-    requestQuestion();
+    //initialize the range-slider on the page
+    var array_of_dom_elements = document.querySelectorAll("input[type=range]");
+    M.Range.init(array_of_dom_elements);
+
+    //fetch all questions from database
+    $.ajax({
+        url: "/api/thebigfive/get",
+        method: "GET"
+    }).done(function (result) {
+        questions = JSON.parse(result);
+        amountOfQuestions = questions.length;
+        console.log(questions);
+        loadQuestion();
+    });
+
 });
 
 // this function gets called as soon as the user presses the "Next" button
 // it's also called once the page loads in order to get the first question
-function requestQuestion(){
+function loadQuestion(){
     //check if there are any questions left
     if(currentQuestion < amountOfQuestions) {
         //when requested, the current question keywords will fade out
         $("#question").fadeOut('fast', function() {
             //while faded out, the next question is set
-            $("#question").text(types[currentQuestion]);
+            $("#question").text(questions[currentQuestion].fragetext);
             //increase the current question counter
             currentQuestion++;
             //set the question count to the current question.
