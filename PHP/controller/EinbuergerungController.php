37820fbc9f6cf045e7d518da11fcc89dd4641990
iamkrisbi <biluskristian@outlook.com>
2020-04-08 10:42:06 +0200

    Bearbeitet

--- PHP/controller/EinbuergerungController.php
+++ PHP/controller/EinbuergerungController.php
@@ -7,8 +7,29 @@ class EinbuergerungController
     {
         $this->EinbuergerungRepository = new EinbuergerungRepository();
     }
-    public function getAllQuestions() {
+
+    public function getQuestionsAndAnswers()
+    {
         $questionArray = $this->EinbuergerungRepository->getAllQuestionsFromEinbuergerung();
         return json_encode($questionArray);
+    }
+
+    public function save($arr)
+    {
+
+        $correctAnswers = $this->EinbuergerungRepository->getCorrectAnswers();
+        $res_id = $this->EinbuergerungRepository->insertResult($_SESSION["uid"], 22, null);
+        $i = 0;
+        $correct = 0;
+        foreach ($arr as $k => $v) {
+            $useranswer_id = $v;
+
+            if ($useranswer_id + 166 == $correctAnswers[$i]["id_antwort"]) $correct++;
+            $this->EinbuergerungRepository->insertUserAnswer($useranswer_id, $_SESSION["uid"], $res_id);
+            $i++;
+        }
+        $this->EinbuergerungRepository->updateGesamtPunktzahl($correct, $res_id);
+
+        Utils::redirect("/evaluation?right=" . $correct . "&wrong=" . (count($correctAnswers) - $correct));
     }
 }
