94ce1529cf5622a96ef3a68764b564ecbb737fd1
JanBloch <jan@bhome.ch>
2020-04-15 15:38:10 +0200

    Added function getNewId() [Utils.php] Added function getNewValidId() [Utils.php]

--- PHP/utilities/Utils.php
+++ PHP/utilities/Utils.php
@@ -3,9 +3,39 @@
 
 class Utils
 {
-    static function redirect($url, $status=302){
+    const alphaNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+    static function redirect($url, $status = 302)
+    {
         http_response_code($status);
         header("Location: $url");
         die();
+    }
+
+    static function getNewValidId()
+    {
+        $sql = "SELECT COUNT(*) FROM gameid WHERE gamecode = ?";
+        while (true) {
+            $id = self::getNewId();
+            $stmt = DB::getInstance()->prepare($sql);
+            $stmt->bind_param("s", $id);
+            if ($stmt->execute()) {
+                $res = $stmt->get_result();
+                if ($res->num_rows) {
+                    if ($res->fetch_row()[0] == 0) {
+                        return $id;
+                    }
+                }
+            }
+        }
+    }
+
+    static function getNewId()
+    {
+        $id = "";
+        for ($i = 0; $i < 6; $i++) {
+            $id .= self::alphaNumeric[random_int(0, strlen(self::alphaNumeric) - 1)];
+        }
+        return $id;
     }
 }
