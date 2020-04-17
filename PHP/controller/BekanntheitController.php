d71f37c8fa997357de193edebd203a9d99e09f2c
1disupt <1disupt@gmail.com>
2020-04-17 13:26:18 +0200

    EinbuergerungController.php = added "public funcion save".

--- PHP/controller/BekanntheitController.php
+++ PHP/controller/BekanntheitController.php
@@ -7,22 +7,4 @@ class BekanntheitController
     {
 
     }
-    public function save($arr)
-    {
-
-        $correctAnswers = $this->BekanntheitRepository->getCorrectAnswers();
-        $res_id = $this->BekanntheitRepository->insertResult($_SESSION["uid"], 21, null);
-        $i = 0;
-        $correct = 0;
-        foreach ($arr as $k => $v) {
-            $useranswer_id = $v;
-
-            if ($useranswer_id + 166 == $correctAnswers[$i]["id_antwort"]) $correct++;
-            $this->BekanntheitRepository->insertUserAnswer($useranswer_id, $_SESSION["uid"], $res_id);
-            $i++;
-        }
-        $msg = $this->BekanntheitRepository->getAnwortText($correct);
-        $this->BekanntheitRepository->updateResultat($correct, $msg[0], $res_id);
-        Utils::redirect("/evaluation?chart=pie&right=" . $correct . "&wrong=" . (count($correctAnswers) - $correct)."&msg=".urlencode($msg[1]));
-    }
 }
