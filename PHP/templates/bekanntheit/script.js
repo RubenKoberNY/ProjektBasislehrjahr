ef9c2fcefc0fe8278153a48ee07b0d40a73200a0
JanBloch <jan@bhome.ch>
2020-04-17 11:05:03 +0200

    Created [bekanntheit/quiz.html] Created [bekanntheit/script.js] Created [bekanntheit/style.css]

--- PHP/templates/bekanntheit/script.js
+++ PHP/templates/bekanntheit/script.js
@@ -1,1 +1,39 @@
+const URLBuilder = {
+    buildURL: function (url, dict) {
+        let out = url;
+        Object.entries(dict).forEach((value, index) => {
+            out += this.getNextURLParam(value[0], value[1], index);
+        });
+        return out;
 
+    },
+    getNextURLParam: function (key, value, count) {
+        if (count == 0)
+            return "?" + key + "=" + value;
+        else
+            return "&" + key + "=" + value;
+    }
+}
+
+var questions;
+var result = [];
+
+var currentIndex = 0;
+
+
+$("#answer1").click(() => {
+    if (currentIndex < questions.length) {
+
+    }
+});
+$("#answer2").click(() => {
+    if (currentIndex < questions.length) {
+
+    }
+});
+$("#answer3").click(() => {
+    if (currentIndex < questions.length) {
+
+
+    }
+});
