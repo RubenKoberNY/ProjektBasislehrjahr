f2dad7817a29bc74d16bd5f8b9e2a2b577570c16
1disupt <1disupt@gmail.com>
2020-05-01 14:12:39 +0200

    fixed something in LerntypController.php

--- PHP/controller/LerntypController.php
+++ PHP/controller/LerntypController.php
@@ -26,46 +26,32 @@ class LerntypController
             switch ($v[0]) {
                 case "A":
                     $au++;
-                    $most = max($au || $vi || $le || $ki);
-                    if ($au == 3) {
-                        $msg = "Sie sind zu 100% ein auditiver Lerntyp!.
-                        Sie lernen durch Zuhören. Stellen Sie Fragen. Diskutieren Sie mit anderen die Themen, die Sie sich merken müssen,
-                        oder tragen Sie Ihr Thema wie ein Mini-Referat laut vor.";
-                    }
                     break;
                 case "V":
                     $vi++;
-                    if ($vi == 3) {
-                        $msg = "Sie sind zu 100% ein visueller Lerntyp!.
-                        Sie lernen durch Beobachtungen. Benutzen Sie Diagramme und Modelle, um Ihre Ideen zu visualisieren.
-                        Ersetzen Sie Schlüsselwörter durch Symbole. Benutzen Sie Farbmarker.";
-                    }
                 case "R":
                     $le++;
-                    if ($le == 3) {
-                        $msg = "Sie sind zu 100% ein lese und schreibe Lerntyp!.
-                        Sie lernen durch Texte. Sie schaffen Klarheit im Denken, indem Sie schreiben.
-                        Erweitern Sie Ihre Notizen beim Abschreiben. Formulieren Sie wichtige Stellen neu.";
-                    }
                     break;
                 case "K":
                     $ki++;
-                    if ($ki == 3) {
-                        $msg = "Sie sind zu 100% ein kinästhetischer Lerntyp!.
-                        Sie lernen durch Ausprobieren.
-                        Benutzen Sie Beispiele, um Ihre Konzepte zu erklären.
-                        Versuchen Sie, sich nicht an Fakten zu erinnern, sondern an Erlebnisse.";
-                    }
                     break;
             }
-            if ($most = 2) {
-                $msg = "Hallo, 66%";
-            } else if ($most == 1)
+            if ($au == 3)
+                {
+                    $msg = "Auditiver Typ";
+                }
+            else if ($le == 3)
             {
-                $msg = "Hallo, 33%";
+                $msg = "Lesen und Schreibe Typ";
             }
-
-
+            else if ($ki == 3)
+            {
+                $msg = "Kinästethischer Typ";
+            }
+            else if ($vi == 3)
+            {
+                $msg = "Visueller Typ";
+            }
             $this->LerntypRepository->insertUserAnswer($this->LerntypRepository->getFrageByAntworttext($v), $_SESSION["uid"], $res_id);
             $i++;
         }
